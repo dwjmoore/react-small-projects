7ef0325b8dbfb2c7b89cffb0aa31e36aeb926907
David Moore <100255970+dwjmoore@users.noreply.github.com>
2023-01-16 19:02:55 +0000

    Created calculator container. Added screen and buttons.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,7 +1,30 @@
 export default function Calculator() {
 	return (
 		<section>
-			<div>The calculator page.</div>
+			<div className="calculator">
+				<div className="calulatorScreen">This is the screen.</div>
+				<div className="buttonsContainer">
+					<button> 1 </button>
+					<button> 2 </button>
+					<button> 3 </button>
+					<button> 4 </button>
+					<button> 5 </button>
+					<button> 6 </button>
+					<button> 7 </button>
+					<button> 8 </button>
+					<button> 9 </button>
+					<button> 0 </button>
+					<button> . </button>
+					<button> = </button>
+					<button> ÷ </button>
+					<button> x </button>
+					<button> - </button>
+					<button> + </button>
+					<button> C </button>
+					<button> AC </button>
+					<button> Delete </button>
+				</div>
+			</div>
 		</section>
 	)
 }
