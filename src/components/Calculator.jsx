394c6dc0ea016bba2b5b4c5cb6906de38454d0a3
David Moore <100255970+dwjmoore@users.noreply.github.com>
2023-01-18 02:27:36 +0000

    Styled the buttons and arranged with css grid.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,25 +1,28 @@
 export default function Calculator() {
 	return (
 		<div className="calculator">
-			<div className="calculatorDisplay">Display</div>
-			<div className="calcButtonsContainer">
-				<button> ÷ </button>
-				<button> x </button>
-				<button> - </button>
-				<button> + </button>
-				<button> 7 </button>
-				<button> 8 </button>
-				<button> 9 </button>
-				<button> 4 </button>
-				<button> 5 </button>
-				<button> 6 </button>
-				<button> 1 </button>
-				<button> 2 </button>
-				<button> 3 </button>
-				<button> 0 </button>
-				<button> . </button>
-				<button> AC </button>
-				<button> = </button>
+			<div className="calculatorDisplay">0</div>
+			<div className="calculatorButtons">
+				<button className="operator">÷</button>
+				<button className="operator">x</button>
+				<button className="operator">-</button>
+				<button className="operator">+</button>
+
+				<button>7</button>
+				<button>8</button>
+				<button>9</button>
+				<button>4</button>
+				<button>5</button>
+				<button>6</button>
+				<button>1</button>
+				<button>2</button>
+				<button>3</button>
+
+				<button>0</button>
+				<button>.</button>
+				<button className="allClear">AC</button>
+
+				<button className="equalSign">=</button>
 			</div>
 		</div>
 	)
