a85f2ec733ace433d6386c1e623c848606fa029f
David Moore <dwjmoore@gmail.com>
2023-01-20 03:31:02 +0000

    added conditionals to buttons event handler to change display state -- number, decimal, and AC.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,36 +1,51 @@
 import { useState } from "react";
 
 export default function Calculator() {
-	const [display, setDisplay] = useState(0);
+	const [display, setDisplay] = useState("0");
 
 	const handleClick = (event) => {
-		console.log(event.target.value);
+
+		if (display === "0" && !isNaN(event.target.textContent)) {
+			setDisplay(event.target.textContent);
+		} else if (!isNaN(event.target.textContent)) {
+			setDisplay(display + event.target.textContent);
+			console.log("The display is non-zero.");
+		}
+
+		if (event.target.textContent === ".") {
+			setDisplay(display + event.target.textContent)
+		}
+
+		if (event.target.textContent === "AC") {
+			setDisplay("0");
+		}
+		
 	}
 
 	return (
 		<div className="calculator">
 			<div className="calculatorDisplay">{display}</div>
 			<div className="calculatorButtons">
-				<button className="operator" onClick={handleClick} value={"÷"}>÷</button>
-				<button className="operator" onClick={handleClick} value={"×"}>×</button>
-				<button className="operator" onClick={handleClick} value={"-"}>-</button>
-				<button className="operator" onClick={handleClick} value={"+"}>+</button>
+				<button className="operator" onClick={handleClick}>÷</button>
+				<button className="operator" onClick={handleClick}>×</button>
+				<button className="operator" onClick={handleClick}>-</button>
+				<button className="operator" onClick={handleClick}>+</button>
 
-				<button onClick={handleClick} value={7}>7</button>
-				<button onClick={handleClick} value={8}>8</button>
-				<button onClick={handleClick} value={9}>9</button>
-				<button onClick={handleClick} value={4}>4</button>
-				<button onClick={handleClick} value={5}>5</button>
-				<button onClick={handleClick} value={6}>6</button>
-				<button onClick={handleClick} value={1}>1</button>
-				<button onClick={handleClick} value={2}>2</button>
-				<button onClick={handleClick} value={3}>3</button>
+				<button onClick={handleClick}>7</button>
+				<button onClick={handleClick}>8</button>
+				<button onClick={handleClick}>9</button>
+				<button onClick={handleClick}>4</button>
+				<button onClick={handleClick}>5</button>
+				<button onClick={handleClick}>6</button>
+				<button onClick={handleClick}>1</button>
+				<button onClick={handleClick}>2</button>
+				<button onClick={handleClick}>3</button>
 
-				<button onClick={handleClick} value={0}>0</button>
-				<button onClick={handleClick} value={"."}>.</button>
-				<button className="allClear" onClick={handleClick} value={"AC"}>AC</button>
+				<button onClick={handleClick}>0</button>
+				<button onClick={handleClick}>.</button>
+				<button className="allClear" onClick={handleClick}>AC</button>
 
-				<button className="equalSign" onClick={handleClick} value={"="}>=</button>
+				<button className="equalSign" onClick={handleClick}>=</button>
 			</div>
 		</div>
 	)
