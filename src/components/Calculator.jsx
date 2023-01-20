9d853057fdaefb079da88fb0fb92c44ddd7f0ff5
David Moore <dwjmoore@gmail.com>
2023-01-20 16:45:08 +0000

    Refactored event listeners for number and decimal butons.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,24 +3,24 @@ import { useState } from "react";
 export default function Calculator() {
 	const [display, setDisplay] = useState("0");
 
-	const handleClick = (event) => {
-
-		if (display === "0" && !isNaN(event.target.textContent)) {
+	const handleNumbers = (event) => {
+		if (display === "0") {
 			setDisplay(event.target.textContent);
-		} else if (!isNaN(event.target.textContent)) {
+		} else {
 			setDisplay(display + event.target.textContent);
 		}
+	}
 
-		if (event.target.textContent === "." && !display.includes(".")) {
+	const handleDecimal = (event) => {
+		if (!display.includes(".")) {
 			setDisplay(display + event.target.textContent)
 		}
-		
 	}
 
-	const handleAllClear = (event) => {
+	const handleAllClear = () => {
 		setDisplay("0");
 	}
-	
+
 	return (
 		<div className="calculator">
 			<div className="calculatorDisplay">{display}</div>
@@ -30,18 +30,18 @@ export default function Calculator() {
 				<button className="operator" onClick={handleClick}>-</button>
 				<button className="operator" onClick={handleClick}>+</button>
 
-				<button onClick={handleClick}>7</button>
-				<button onClick={handleClick}>8</button>
-				<button onClick={handleClick}>9</button>
-				<button onClick={handleClick}>4</button>
-				<button onClick={handleClick}>5</button>
-				<button onClick={handleClick}>6</button>
-				<button onClick={handleClick}>1</button>
-				<button onClick={handleClick}>2</button>
-				<button onClick={handleClick}>3</button>
+				<button onClick={handleNumbers}>7</button>
+				<button onClick={handleNumbers}>8</button>
+				<button onClick={handleNumbers}>9</button>
+				<button onClick={handleNumbers}>4</button>
+				<button onClick={handleNumbers}>5</button>
+				<button onClick={handleNumbers}>6</button>
+				<button onClick={handleNumbers}>1</button>
+				<button onClick={handleNumbers}>2</button>
+				<button onClick={handleNumbers}>3</button>
+				<button onClick={handleNumbers}>0</button>
 
-				<button onClick={handleClick}>0</button>
-				<button onClick={handleClick}>.</button>
+				<button onClick={handleDecimal}>.</button>
 				<button className="allClear" onClick={handleAllClear}>AC</button>
 
 				<button className="equalSign" onClick={handleClick}>=</button>
