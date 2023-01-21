f7f58d1975250d57a40b2ade1236016d2c7d5ba3
David Moore <dwjmoore@gmail.com>
2023-01-21 21:37:47 +0000

    Refactored add function. Refactored conditionals with return statements.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,77 +1,107 @@
-// ---Display
-// ---The number buttons
-// ---All Clear
-// Operators
+// xxxHitting numbers to show up on display.
+// xxxAdding the decimal to a number on the display.
+// xxxAll Clear
+// add
+// subtract
+// multiply
+// divide
+
 
 import { useState } from "react";
 
 export default function Calculator() {
 	const [display, setDisplay] = useState("0");
-	const [equationPartOne, setEquationPartOne] = useState(null);
-	const [addOn, setAddOn] = useState(false);
+	const [baseMemory, setBaseMemory] = useState(null);
+	const [addMemory, setAddMemory] = useState(null);
+	const [add, setAdd] = useState("OFF");
+
+	const handleAdd = () => {
+		if (add === "OFF") {
+			setAdd("ON");
+			setBaseMemory(display);
+		}
+
+		if (add === "ON") {
+			setAddMemory(null);
+		}
+	}
 
 	const handleNumbers = (event) => {
 		if (display === "0") {
 			setDisplay(event.target.textContent);
-		} else {
-			setDisplay(display + event.target.textContent);
+			return;
 		}
 
-		if (addOn) {
-			if (equationPartOne) {
-				setDisplay(display + event.target.textContent);
-			} else {
-				setEquationPartOne(display);
-				setDisplay(event.target.textContent);
-			}
+		if (add === "ON" && !addMemory) {
+			setDisplay(event.target.textContent);
+			setAddMemory(event.target.textContent);
+			return;
 		}
+
+		if (add === "ON" && addMemory) {
+			setDisplay(display + event.target.textContent);
+			setAddMemory(addMemory + event.target.textContent);
+		}
+
+		setDisplay(display + event.target.textContent);
 	}
 
 	const handleDecimal = (event) => {
-		if (!display.includes(".")) {
-			setDisplay(display + event.target.textContent)
+		if (baseMemory && add === "ON" && !addMemory) {
+			setDisplay("0" + event.target.textContent);
+			setAddMemory("0" + event.target.textContent);
+			return;
 		}
-	}
 
-	const handleAdd = () => {
-		setAddOn(true);
+		if (!display.includes(".")) {
+			setDisplay(display + event.target.textContent);
+			return;
+		}
 	}
 
 	const handleAllClear = () => {
 		setDisplay("0");
-		setEquationPartOne(null);
+		setBaseMemory(null);
+		setAddMemory(null);
+		setAdd("OFF");
 	}
 
 	const handleEqualSign = () => {
-		if (addOn) {
-				setDisplay((Number(equationPartOne) + Number(display)).toString())
+		if (add === "ON" && addMemory) {
+			setDisplay((Number(baseMemory) + Number(addMemory)).toString());
+			setBaseMemory((Number(baseMemory) + Number(addMemory)).toString());
 		}
 	}
 
 	return (
-		<div className="calculator">
-			<div className="calculatorDisplay">{display}</div>
-			<div className="calculatorButtons">
-				<button className="operator" onClick={handleAdd}>+</button>
-				<button className="operator">-</button>
-				<button className="operator">×</button>
-				<button className="operator">÷</button>
+		<div>
+			<h2>Add: {add}</h2>
+			<h2>Base Memory: {baseMemory}</h2>
+			<h2>Add Memory: {addMemory}</h2>
+			<div className="calculator">
+				<div className="calculatorDisplay">{display}</div>
+				<div className="calculatorButtons">
+					<button className="operator" onClick={handleAdd}>+</button>
+					<button className="operator">-</button>
+					<button className="operator">×</button>
+					<button className="operator">÷</button>
 
-				<button onClick={handleNumbers}>7</button>
-				<button onClick={handleNumbers}>8</button>
-				<button onClick={handleNumbers}>9</button>
-				<button onClick={handleNumbers}>4</button>
-				<button onClick={handleNumbers}>5</button>
-				<button onClick={handleNumbers}>6</button>
-				<button onClick={handleNumbers}>1</button>
-				<button onClick={handleNumbers}>2</button>
-				<button onClick={handleNumbers}>3</button>
-				<button onClick={handleNumbers}>0</button>
+					<button onClick={handleNumbers}>7</button>
+					<button onClick={handleNumbers}>8</button>
+					<button onClick={handleNumbers}>9</button>
+					<button onClick={handleNumbers}>4</button>
+					<button onClick={handleNumbers}>5</button>
+					<button onClick={handleNumbers}>6</button>
+					<button onClick={handleNumbers}>1</button>
+					<button onClick={handleNumbers}>2</button>
+					<button onClick={handleNumbers}>3</button>
+					<button onClick={handleNumbers}>0</button>
 
-				<button onClick={handleDecimal}>.</button>
-				<button className="allClear" onClick={handleAllClear}>AC</button>
+					<button onClick={handleDecimal}>.</button>
+					<button className="allClear" onClick={handleAllClear}>AC</button>
 
-				<button className="equalSign" onClick={handleEqualSign}>=</button>
+					<button className="equalSign" onClick={handleEqualSign}>=</button>
+				</div>
 			</div>
 		</div>
 	)
